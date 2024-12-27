2444e3072e79792c273d25797d3491ffcb9cc21f
Arly Moura <arlymoura@gmail.com>
2024-12-27 14:01:02 -0300

    Set default value for is_virtual column in tables migration

--- db/migrate/20241227163450_create_tables.rb
+++ db/migrate/20241227163450_create_tables.rb
@@ -3,7 +3,7 @@ class CreateTables < ActiveRecord::Migration[7.0]
     create_table :tables, id: :uuid do |t|
       t.integer :number
       t.integer :status
-      t.boolean :is_virtual
+      t.boolean :is_virtual, default: false, null: false
 
       t.timestamps
     end
