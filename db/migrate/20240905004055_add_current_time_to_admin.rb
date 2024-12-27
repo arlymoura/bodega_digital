51a06c7933ca0ec53ef8fd2f273ecf644aa06c70
Arly Moura <arlymoura@gmail.com>
2024-12-27 13:59:10 -0300

    Remove Admin model and related migrations

--- db/migrate/20240905004055_add_current_time_to_admin.rb
+++
@@ -1,5 +0,0 @@
-class AddCurrentTimeToAdmin < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :admins, :current_time, type: :uuid, foreign_key: { to_table: :teams }, null: true
-  end
-end
